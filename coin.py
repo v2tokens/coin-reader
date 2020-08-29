43db6059c639a3562952f912f372718c25e1adc6
Luke Murphy <lukewm@riseup.net>
2020-08-29 14:54:50 +0200

    Add KeyboardInterrupt catch and clean

--- coin.py
+++ coin.py
@@ -14,15 +14,16 @@ SCREEN1_URL = "http://screen1.home:5000"
 counter = 0
 COUNTER_GOAL = 30
 
-while 1:
-    if GPIO.input(NUM_PIN) == 0:
-        get(SCREEN1_URL)
+try:
+    while 1:
+        if GPIO.input(NUM_PIN) == 0:
+            get(SCREEN1_URL)
 
-        counter += 1
-        if counter == COUNTER_GOAL:
-            get(TICKER_URL)
-            counter = 0
+            counter += 1
+            if counter == COUNTER_GOAL:
+                get(TICKER_URL)
+                counter = 0
 
-    time.sleep(0.1)
-
-GPIO.cleanup()
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
