246b08437026e69a0db35574908ccbfa5952784f
Luke Murphy <lukewm@riseup.net>
2020-09-01 12:19:05 +0200

    Print and spacing

--- app.py
+++ app.py
@@ -9,21 +9,27 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(NUM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 TICKER_URL = "http://ticker:5000"
-SCREEN1_URL = "http://MBP-van-Lotte:5000"
+SCREEN1_URL = "http://mbp-van-lotte:5000"
 SCREEN2_URL = "http://screen2:5000"
 
 counter = 0
 COUNTER_GOAL = 30
 
+print("Running GPIO read loop...")
+
 try:
     while 1:
         if GPIO.input(NUM_PIN) == 0:
+            print("Coin detected...")
+
             get(SCREEN1_URL)
             get(SCREEN2_URL)
+
             counter += 1
             if counter == COUNTER_GOAL:
                 get(TICKER_URL)
                 counter = 0
+
         time.sleep(0.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
