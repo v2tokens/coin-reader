6f81d074b8bc14b5f7368bec43666a7868fdadb7
Luke Murphy <lukewm@riseup.net>
2020-09-01 12:27:39 +0200

    Adding start-up tests

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import time
+from sys import exit
 
 from requests import get
 from RPi import GPIO as GPIO
@@ -8,26 +9,47 @@ NUM_PIN = 37
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(NUM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-TICKER_URL = "http://ticker:5000"
-SCREEN1_URL = "http://mbp-van-lotte:5000"
-SCREEN2_URL = "http://screen2:5000"
+DEFAULT_PORT = 5000
+TICKER_HOST = "ticker"
+SCREEN1_HOST = "mbp-van-lotte"
+SCREEN2_HOST = "screen2"
 
 counter = 0
 COUNTER_GOAL = 30
 
-print("Running GPIO read loop...")
+
+def fire_request(url):
+    try:
+        get(url)
+        return True
+    except Exception:
+        print(f"Failed to request {url}...")
+        return False
+
+
+def go_test():
+    for host in [TICKER_HOST, SCREEN1_HOST, SCREEN2_HOST]:
+        success = fire_request(f"http://{TICKER_HOST}/ok:{DEFAULT_PORT}")
+        if not success:
+            print(f"Unable to contact {host}, bailing out...")
+            exit(1)
+
+
+print("Running start-up tests...")
+go_test()
 
 try:
+    print("Running GPIO read loop...")
     while 1:
         if GPIO.input(NUM_PIN) == 0:
             print("Coin detected...")
 
-            get(SCREEN1_URL)
-            get(SCREEN2_URL)
+            fire_request(f"http://{SCREEN1_HOST}:{DEFAULT_PORT}")
+            fire_request(f"http://{SCREEN2_HOST}:{DEFAULT_PORT}")
 
             counter += 1
             if counter == COUNTER_GOAL:
-                get(TICKER_URL)
+                fire_request(f"http://{TICKER_HOST}:{DEFAULT_PORT}")
                 counter = 0
 
         time.sleep(0.1)
